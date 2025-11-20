27efe1d6e840d009e00f81233ab070341ed1367f
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-11-20 14:10:46 +0900

    文字列に修正

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,8 +1,8 @@
 export type Recipe = {
   id: string;
   title: string;
-  materials: string;  // ← 配列ではなく文字列
-  steps: string;      // ← 同じく文字列
+  materials: string;  // 配列ではなく文字列
+  steps: string;      // 同じく文字列
   image?: {
     url: string;
   };
