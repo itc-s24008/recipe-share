ffaada3875b5c769d502f9c578bcd80c73edd313
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-11-20 15:04:01 +0900

    画像の修正

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { client } from "../../../lib/client";
 import { Recipe } from "../../../types/recipe";
+import Image from "next/image";
 
-export default async function RecipeDetail({ params }: { params: Promise<{ id: string }> }) {
+export default async function RecipeDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
 
   const recipe: Recipe = await client.get({
@@ -12,11 +17,22 @@ export default async function RecipeDetail({ params }: { params: Promise<{ id: s
   return (
     <article>
       <h1>{recipe.title}</h1>
-      {recipe.image && <img src={recipe.image.url} alt={recipe.title} />}
+
+      {recipe.image && (
+        <Image
+          src={recipe.image.url}
+          alt={recipe.title}
+          width={800}
+          height={500}
+          style={{ width: "100%", height: "auto" }}
+        />
+      )}
+
       <section>
         <h2>材料</h2>
         <div dangerouslySetInnerHTML={{ __html: recipe.materials }} />
       </section>
+
       <section>
         <h2>手順</h2>
         <div dangerouslySetInnerHTML={{ __html: recipe.steps }} />
