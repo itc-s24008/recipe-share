b8624fbb93e751de7be6cb32c39714214de4dd81
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-11-20 16:39:38 +0900

    画像を修正

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,9 +1,9 @@
 export type Recipe = {
   id: string;
   title: string;
-  materials: string;  // 配列ではなく文字列
-  steps: string;      // 同じく文字列
+  materials: string;  
+  steps: string;  
   image?: {
     url: string;
-  };
+  } | null;  
 };
