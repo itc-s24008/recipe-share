378dc4d6992fbff5d115dc8efc41372225ff2241
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-11-26 10:17:24 +0900

    レシピ一覧のフォームを変えた

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,32 +1,24 @@
-import Link from "next/link";
 import Image from "next/image";
-import styles from "./RecipeCard.module.css";
+import Link from "next/link";
 import { Recipe } from "../types/recipe";
+import styles from "./RecipeCard.module.css";
 
 export default function RecipeCard({ recipe }: { recipe: Recipe }) {
-  const materialsArray = recipe.materials?.split("\n") || [];
-
   return (
     <Link href={`/recipes/${recipe.id}`} className={styles.card}>
-      {recipe.image?.url && (
+      {recipe.image && (
         <Image
           src={recipe.image.url}
           alt={recipe.title}
-          width={400}     
-          height={300}    
-          className={styles.image}
+          width={400}
+          height={250}
+          className={styles.thumbnail}
         />
       )}
 
-      <div className={styles.content}>
-        <h3 className={styles.title}>{recipe.title}</h3>
+      <h3 className={styles.title}>{recipe.title}</h3>
 
-        <p className={styles.materials}>
-          {materialsArray.length > 0
-            ? materialsArray.slice(0, 3).join("、")
-            : "材料情報なし"}
-        </p>
-      </div>
+
     </Link>
   );
 }
